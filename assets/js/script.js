07aa822465b9987a196de789e12287b4a83fec5b
carlincb <carlin.colleen@gmail.com>
2021-09-19 19:55:04 -0600

    Defined global variables.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,56 @@
-// Define global variables
-var startQuizButton = document.getElementById("#startQuizButton");
+// Defined global variables.
+var startQuizButton = document.getElementById("startQuizButton");
 
-// Initialize Start Quiz Button
+// Variable defined for timer/score input.
+var timerInput = document.getElementById("countdown");
+
+// Variables defined for questions.
+var question1 = document.getElementById("question1");
+var question2 = document.getElementById("question2");
+var question3 = document.getElementById("question3");
+var question4 = document.getElementById("question4");
+
+// Variables defined for button options for answers and feedback on answers.
+var btn1A = document.getElementById("option1A");
+var btn1B = document.getElementById("option1B");
+var btn1C = document.getElementById("option1C");
+var btn1D = document.getElementById("option1D");
+var answerFeedback1 = document.getElementById("answerFeedback1");
+
+var btn2A = document.getElementById("option2A");
+var btn2B = document.getElementById("option2B");
+var btn2C = document.getElementById("option2C");
+var btn2D = document.getElementById("option2D");
+var answerFeedback2 = document.getElementById("answerFeedback2");
+
+var btn3A = document.getElementById("option3A");
+var btn3B = document.getElementById("option3B");
+var btn3C = document.getElementById("option3C");
+var btn3D = document.getElementById("option3D");
+var answerFeedback3 = document.getElementById("answerFeedback3");
+
+var btn4A = document.getElementById("option4A");
+var btn4B = document.getElementById("option4B");
+var btn4C = document.getElementById("option4C");
+var btn4D = document.getElementById("option4D");
+var answerFeedback4 = document.getElementById("answerFeedback4");
+
+// Defined variables for page display after the quiz runs.
+var finalScorePage = document.getElementById("finalScore");
+var highScorePage = document.getElementById("highscores");
+var scoreInput = document.getElementById("score");
+
+var submitButton = document.getElementById("submitButton");
+var goBackButton = document.getElementById("goBackButton");
+var clearHighScores = document.getElementById("clearHighscores");
+
+// Start Quiz Button Linked
+startQuizButton.addEventListener("click", function(){
+
+});
 
 // Create Timer Function
+
 // Create Question/Answer/Feedback Function
 // Create Score Tracker Function
-// Create Function to Save/Clear High Scores
+// Create Function to Save/Clear Highscores
