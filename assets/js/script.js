6ab7a62688d2a0356a03f42c21dcd7ece9e1217a
carlincb <carlin.colleen@gmail.com>
2021-09-19 19:04:55 -0600

    Added score section and span tags for time-remaining and scores. Added more ids for easy manipulation from js file.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,9 @@
 // Define global variables
 var startQuizButton = document.getElementById("#startQuizButton");
+
+// Initialize Start Quiz Button
+
+// Create Timer Function
+// Create Question/Answer/Feedback Function
+// Create Score Tracker Function
+// Create Function to Save/Clear High Scores
